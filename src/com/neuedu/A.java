8758518547ed14ae4e1686f2353da1bad744205b
田艳 <1454611005@qq.com>
2019-05-24 08:37:21 +0800

    1

---
+++ src/com/neuedu/A.java
@@ -0,0 +1,14 @@
+package com.neuedu;
+
+public class A {
+    public static void main(String[] args) {
+        int i=456;
+        int f=0;
+        while (i !=0){
+            f=i%10;
+            System.out.print(f);
+            i=i/10;
+
+        }
+    }
+}
